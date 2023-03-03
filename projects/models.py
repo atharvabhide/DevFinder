329cc9f6516dccecd4aa619287805591c48a08df
atharvabhide <atharvabhide91@gmail.com>
2023-03-03 15:35:05 +0530

    add models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote')
+    )
+    # owner = 
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
 # Create your views here.
 def projects(request):
-    return render(request, 'projects/projects.html', {})
+    page = "projects"
+    number = 10
+    context = {'page' : page, 'number' : number, 'projects' : projectsList}
+    return render(request, 'projects/projects.html', context)
 
-def project(request, pk:int):
-    return render(request, 'projects/single-project.html', {})
+def project(request, pk):
+    project = None
+    for i in (projectsList):
+        if (int(i['id']) == pk):
+            project = i
+    if (project != None):
+        context = {'project' : project}
+        return render(request, 'projects/single-project.html', context)
+    else:
+        return HttpResponse("Project not found!")
