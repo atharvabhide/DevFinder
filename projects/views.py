f4f435f806449516cf92ddb9dc1f8446475312e2
atharvabhide <atharvabhide91@gmail.com>
2023-04-02 19:42:44 +0530

    add user project crud

--- projects/views.py
+++ projects/views.py
@@ -23,13 +23,16 @@ def project(request, pk:str):
 
 @login_required(login_url='login')
 def createProject(request):
+    profile = request.user.profile
     # creates a new project
     form = ProjectForm()
 
     if (request.method == 'POST'):
         form = ProjectForm(request.POST, request.FILES)
         if (form.is_valid()):
-            form.save()
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
             return redirect('projects')
     context = {
         'form': form,   
@@ -38,8 +41,9 @@ def createProject(request):
 
 @login_required(login_url='login')
 def updateProject(request, pk):
+    profile = request.user.profile
     # updates a project
-    project = Project.objects.get(id=pk)
+    project = profile.project_set.get(id=pk)
     form = ProjectForm(instance=project)
 
     if (request.method == 'POST'):
@@ -54,8 +58,9 @@ def updateProject(request, pk):
 
 @login_required(login_url='login')
 def deleteProject(request, pk):
+    profile = request.user.profile
     # deletes a project
-    project = Project.objects.get(id=pk)
+    project = profile.project_set.get(id=pk)
 
     if (request.method == 'POST'):
         project.delete()
