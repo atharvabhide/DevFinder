47cc0ac0281682d0c23d9182d43a9439893b9bac
Aakash <aakashkasabekar@gmail.com>
2023-04-07 20:44:32 +0530

    feat: add Navbar UI

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import {GoSearch} from 'react-icons/go'
+import {HiMenuAlt3} from 'react-icons/hi'
+import {Link} from 'react-router-dom'
+import {HiCode, HiOutlineHome} from 'react-icons/hi'
+import {MdOutlineLogin} from 'react-icons/md'
+import {TbMessageCircle} from 'react-icons/tb'
+import {FiUsers} from 'react-icons/fi'
+import {MdClose} from 'react-icons/md'
+
+
+const Navbar = () => {
+    const IconStyle = {color: "white"}
+    const IconstyleMenu = {color: "#333"}
+
+    const [navbarOpen, setNavbarOpen] = useState(false);
+    const handleToggleClick = () => {
+        setNavbarOpen((prev) => !prev)
+        console.log("click ho gaya");
+    }
+
+
+    
+  return (
+    <>
+        <header className="header">
+            <div className="headerContainer">
+                
+                <h3>DevFinder</h3>
+
+                <div className="headerSearch">
+                    <input type="text" placeholder='Search' className="headerInput" />
+                    <GoSearch style={IconStyle} />
+
+                </div>
+
+                {/* <img Logo idhar** /> */}
+
+                <button className="headerToggle" onClick={handleToggleClick} >
+
+                    {navbarOpen ? <MdClose style={IconstyleMenu} size={25} /> : <HiMenuAlt3 style={IconstyleMenu} size={25} />}
+                </button>
+                
+            </div>
+        </header>
+        <div className="nav" >
+            <nav className={`navContainer ${navbarOpen ? 'navOpen' : 'navClosed'}`}>
+                <div >
+                
+                    <Link className="navLink navLogo"><span className="navLogoName">DevFinder</span></Link>
+
+                    <div className="navList">
+                        <div className="navItems"  >
+                            <h3 className="navSubTitle">Menu</h3>
+
+                            <Link className="navLink ">
+                                <HiOutlineHome className="navIcon"/>
+                                <span className="navName">Home</span>
+                            </Link>
+
+                            <Link className="navLink">
+                                <FiUsers className="navIcon" />
+                                <span className="navName">Developers</span>
+                            </Link>
+
+                            <Link className="navLink">
+                                <HiCode className="navIcon"/>
+                                <span className="navName">Projects</span>
+                            </Link>
+
+                            <Link className="navLink">
+                                <TbMessageCircle className="navIcon" />
+                                <span className="navName">Inbox</span>
+                            </Link>
+
+                            <Link className="navLink">
+                                <MdOutlineLogin className="navIcon" />
+                                <span className="navName">Login/Signup</span>
+                            </Link> 
+
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
