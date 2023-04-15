8d026b5cf429fa6be43bfd3c91ff4291a90f3218
Aakash <aakashkasabekar@gmail.com>
2023-04-15 20:40:09 +0530

    feat: Add Project cards

---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from './ProjectCard.module.css'
+import Banner from '../../assets/banner2.jpg'
+
+export const ProjectCard = (props) => {
+  return (
+    <>
+        <div className={styles.card}>
+            <div className={styles.banner}>
+                <img className={styles.projectImage} src={Banner} alt="" />
+            </div>
+            <ul>
+                <li className={styles.projectTitle}><b>{props.projectName}</b></li>
+                <li className={styles.projectDeveloper}>by {props.projectDeveloper}</li>
+                <li className={styles.projectFeedback}>{props.projectFeedback} Positive Feedback({props.voteCount} votes)</li>
+            </ul>
+            <div className={styles.bottom}>
+            <div className={styles.toolSection}>
+                <button className={styles.toolBtn} style={props.tool1 ? {backgroundColor: '#5670d7'} : {display: 'none'}}>{props.tool1}</button>
+                <button className={styles.toolBtn} style={props.tool2 ? {backgroundColor: '#5670d7'} : {display: 'none'}}>{props.tool2}</button>
+                <button className={styles.toolBtn} style={props.tool3 ? {backgroundColor: '#5670d7'} : {display: 'none'}}>{props.tool3}</button>
+                <button className={styles.toolBtn} style={props.tool4 ? {backgroundColor: '#5670d7'} : {display: 'none'}}>{props.tool4}</button>
+            </div>
+            </div>
+        </div>
+    </>
+  )
+}
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,7 +1,51 @@
 import React from 'react'
+import styles from './Projects.module.css'
+import { ProjectCard } from '../../components/ProjectCard/ProjectCard'
 
 export const Projects = () => {
   return (
-    <div>Projects</div>
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.header}>
+
+        </div>
+        <div className={styles.projects}>
+
+          <ProjectCard
+            projectName="Portfolio"
+            projectDeveloper="wolfmartel"
+            projectFeedback="78%"
+            voteCount="80"
+            
+            tool1="React"
+            tool2="MongoDB"
+            tool3="Node.js"
+            tool4="Express.js"
+          />
+          <ProjectCard
+            projectName="Portfolio"
+            projectDeveloper="wolfmartel"
+            projectFeedback="78%"
+            voteCount="80"
+            
+            tool1="React"
+            tool2="MongoDB"
+            tool3="Node.js"
+            tool4="Express.js"
+          />
+          <ProjectCard
+            projectName="Portfolio"
+            projectDeveloper="wolfmartel"
+            projectFeedback="78%"
+            voteCount="80"
+            
+            tool1="React"
+            tool2="MongoDB"
+            tool3="Node.js"
+            tool4="Express.js"
+          />
+        </div>
+      </div>
+    </>
   )
 }
