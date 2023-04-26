4cb87b301daef87af0ec4f6787e146b5066f188d
atharvabhide <atharvabhide91@gmail.com>
2023-04-26 23:35:30 +0530

    fix: jwt auth fix

--- devfinder/settings.py
+++ devfinder/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'user_api',
     'project_api',
     'corsheaders',
+    'rest_framework_simplejwt',
 ]
 
 CORS_ORIGIN_WHITELIST = (
@@ -141,4 +143,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
 }
