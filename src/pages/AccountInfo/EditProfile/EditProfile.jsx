d5065696d180bc278abefdae2da138a27ae751b1
Aryan Sawale <aryansawale3@gmail.com>
2023-05-05 01:42:59 +0530

    add NSFW detection in profile pic upload

--- src/pages/AccountInfo/EditProfile/EditProfile.jsx
+++ src/pages/AccountInfo/EditProfile/EditProfile.jsx
@@ -1,45 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import styles from './EditProfile.module.css'
 import { useAxios } from '../../../utils/useAxios';
+import axios from 'axios';
 
 export const EditProfile = () => {
 
   const [selectedFile, setSelectedFile] = useState();
-  const [imageData, setImageData] = useState();
 
   const api = useAxios();
-
-  const handlePic = async () => {
-    const formData = new FormData();
-    formData.append("image", selectedFile);
-    console.log(formData);
-    for (const pair of formData.entries()) {
-      console.log(`${pair[0]}, ${pair[1]}`);
-    }
-    for (const value of formData.values()) {
-      console.log("value ", value);
-    }
-    setImageData(formData);
-  }
-
-  // useEffect(() => {
-  //   handlePic();
-  // }, [selectedFile])
-  
-  const testData = {
-    "name": "Testing",
-    "email": "testingbro@gmail.com",
-    "username": "Testerbro",
-    "location": "Your mom's house",
-    "bio": "I dont really like biology",
-    "profileImage": imageData,
-    "socialGithub": "https://github.com/Aryan-Sawale",
-    "socialLinkedIn": "https://github.com/Aryan-Sawale",
-    "socialTwitter": "https://github.com/Aryan-Sawale",
-    "socialYoutube": "https://github.com/Aryan-Sawale",
-    "socialWebsite": "https://github.com/Aryan-Sawale",
-    "skills": [],
-  }
 
   const updateProfile = async () => {
     const formData = new FormData();
@@ -55,7 +23,6 @@ export const EditProfile = () => {
     formData.append("socialYoutube", "https://github.com/Aryan-Sawale");
     formData.append("socialWebsite", "https://github.com/Aryan-Sawale");
     // formData.append("skills", []);
-
     console.log(formData);
     for (const pair of formData.entries()) {
       console.log(`${pair[0]}, ${pair[1]}`);
@@ -63,22 +30,23 @@ export const EditProfile = () => {
     for (const value of formData.values()) {
       console.log("value ", value);
     }
-    // const testData = {
-    //   "name": "Testing",
-    //   "email": "testingbro@gmail.com",
-    //   "username": "Testerbro",
-    //   "location": "Your mom's house",
-    //   "bio": "I dont really like biology",
-    //   "profileImage": formData,
-    //   "socialGithub": "https://github.com/Aryan-Sawale",
-    //   "socialLinkedIn": "https://github.com/Aryan-Sawale",
-    //   "socialTwitter": "https://github.com/Aryan-Sawale",
-    //   "socialYoutube": "https://github.com/Aryan-Sawale",
-    //   "socialWebsite": "https://github.com/Aryan-Sawale",
-    //   "skills": [],
-    // }
-    const response = await api.post("http://127.0.0.1:8000/user-api/profiles/c48554e6-f19b-4fb5-900c-2afe6573323f/update/", formData);
-    console.log(response);
+
+    const imageData = new FormData();
+    imageData.append("image", selectedFile);
+    for (const value of imageData.values()) {
+      console.log("value ", value);
+    }
+    const responseNSFW = await api.post("http://localhost:8000/project-api/image/mod/", imageData);
+    console.log(responseNSFW);
+
+    if (responseNSFW.data.prediction != "image is nsfw")
+    {
+      const response = await api.post("http://127.0.0.1:8000/user-api/profiles/c48554e6-f19b-4fb5-900c-2afe6573323f/update/", formData);
+      console.log(response);
+    }
+    else {
+      alert("No NSFW images allowed")
+    }
   }
 
   return (
@@ -91,7 +59,10 @@ export const EditProfile = () => {
             <input type="text" className={styles.inputField} placeholder='Location' />
             <textarea className={styles.bio} name="Bio" id="" cols="30" rows="10" placeholder='Bio'></textarea>
             <input type="text" className={styles.inputField} placeholder='Short Intro'  />
-            <input type="file" onChange={(e) => {console.log(e.target.files[0]); setSelectedFile(e.target.files[0]);} }/> 
+            <div className={styles.upload_container}>
+              <label>Profile Image</label>
+              <input type="file" onChange={(e) => {console.log(e.target.files[0]); setSelectedFile(e.target.files[0]);} }/> 
+            </div>
             <input type="url" className={styles.inputField} placeholder='Github Profile'  />
             <input type="url" className={styles.inputField} placeholder='LinkedIn Profile'  />
             <input type="url" className={styles.inputField} placeholder='Twitter Profile'  />
