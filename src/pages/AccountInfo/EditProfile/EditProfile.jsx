aab0553ce3e7995cb571798a67b903f6f4e78f46
Aryan Sawale <aryansawale3@gmail.com>
2023-05-05 00:50:36 +0530

    edit profile and reviews create

--- src/pages/AccountInfo/EditProfile/EditProfile.jsx
+++ src/pages/AccountInfo/EditProfile/EditProfile.jsx
@@ -1,10 +1,89 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './EditProfile.module.css'
+import { useAxios } from '../../../utils/useAxios';
 
 export const EditProfile = () => {
+
+  const [selectedFile, setSelectedFile] = useState();
+  const [imageData, setImageData] = useState();
+
+  const api = useAxios();
+
+  const handlePic = async () => {
+    const formData = new FormData();
+    formData.append("image", selectedFile);
+    console.log(formData);
+    for (const pair of formData.entries()) {
+      console.log(`${pair[0]}, ${pair[1]}`);
+    }
+    for (const value of formData.values()) {
+      console.log("value ", value);
+    }
+    setImageData(formData);
+  }
+
+  // useEffect(() => {
+  //   handlePic();
+  // }, [selectedFile])
+  
+  const testData = {
+    "name": "Testing",
+    "email": "testingbro@gmail.com",
+    "username": "Testerbro",
+    "location": "Your mom's house",
+    "bio": "I dont really like biology",
+    "profileImage": imageData,
+    "socialGithub": "https://github.com/Aryan-Sawale",
+    "socialLinkedIn": "https://github.com/Aryan-Sawale",
+    "socialTwitter": "https://github.com/Aryan-Sawale",
+    "socialYoutube": "https://github.com/Aryan-Sawale",
+    "socialWebsite": "https://github.com/Aryan-Sawale",
+    "skills": [],
+  }
+
+  const updateProfile = async () => {
+    const formData = new FormData();
+    formData.append("profileImage", selectedFile);
+    formData.append("name", "Testing");
+    formData.append("email", "testingbro@gmail.com");
+    formData.append("username", "Testerbro");
+    formData.append("location", "Your mom's house");
+    formData.append("bio", "I dont really like biology");
+    formData.append("socialGithub", "https://github.com/Aryan-Sawale");
+    formData.append("socialLinkedIn", "https://github.com/Aryan-Sawale");
+    formData.append("socialTwitter", "https://github.com/Aryan-Sawale");
+    formData.append("socialYoutube", "https://github.com/Aryan-Sawale");
+    formData.append("socialWebsite", "https://github.com/Aryan-Sawale");
+    // formData.append("skills", []);
+
+    console.log(formData);
+    for (const pair of formData.entries()) {
+      console.log(`${pair[0]}, ${pair[1]}`);
+    }
+    for (const value of formData.values()) {
+      console.log("value ", value);
+    }
+    // const testData = {
+    //   "name": "Testing",
+    //   "email": "testingbro@gmail.com",
+    //   "username": "Testerbro",
+    //   "location": "Your mom's house",
+    //   "bio": "I dont really like biology",
+    //   "profileImage": formData,
+    //   "socialGithub": "https://github.com/Aryan-Sawale",
+    //   "socialLinkedIn": "https://github.com/Aryan-Sawale",
+    //   "socialTwitter": "https://github.com/Aryan-Sawale",
+    //   "socialYoutube": "https://github.com/Aryan-Sawale",
+    //   "socialWebsite": "https://github.com/Aryan-Sawale",
+    //   "skills": [],
+    // }
+    const response = await api.post("http://127.0.0.1:8000/user-api/profiles/c48554e6-f19b-4fb5-900c-2afe6573323f/update/", formData);
+    console.log(response);
+  }
+
   return (
     <div className={styles.wrapper}>
-        <form className={styles.form} action="#" method=''>
+        <form className={styles.form} action="#" method='' onSubmit={(e) => {e.preventDefault();}}>
             <p className={styles.formTitle}>Edit Profile</p>
             <input type="text" className={styles.inputField} placeholder='Name' />
             <input type="email" className={styles.inputField} placeholder='Email' />
@@ -12,12 +91,13 @@ export const EditProfile = () => {
             <input type="text" className={styles.inputField} placeholder='Location' />
             <textarea className={styles.bio} name="Bio" id="" cols="30" rows="10" placeholder='Bio'></textarea>
             <input type="text" className={styles.inputField} placeholder='Short Intro'  />
+            <input type="file" onChange={(e) => {console.log(e.target.files[0]); setSelectedFile(e.target.files[0]);} }/> 
             <input type="url" className={styles.inputField} placeholder='Github Profile'  />
             <input type="url" className={styles.inputField} placeholder='LinkedIn Profile'  />
             <input type="url" className={styles.inputField} placeholder='Twitter Profile'  />
             <input type="url" className={styles.inputField} placeholder='Youtube Page'  />
             <input type="url" className={styles.inputField} placeholder='Portfolio'  /><br />
-            <input type="submit" className={styles.submitButton} />
+            <input type="submit" className={styles.submitButton} onClick={updateProfile}/>
 
 
         </form>
--- src/pages/Projects/IndividualProject/IndividualProject.jsx
+++ src/pages/Projects/IndividualProject/IndividualProject.jsx
@@ -20,42 +20,55 @@ export const IndividualProject = (props) => {
     const [reviews, setReviews] = useState([]);
 
     // console.log(location);
-    const url = location.state.url;
+    const url = location.state?.url;
     // console.log(url);
 
     const api = useAxios();
     
   
-
   const fetchProject = async () => {
     try {
-        const response = await api.get(url);
-        console.log(response);
-        const project = response.data;
-        setMyData(project);
-        
-        console.log(`${url}reviews/`);
-        const reviewData = await api.get(`${url}reviews/`)
-        console.log(reviewData.data.results);
-        setReviews(reviewData.data.results)
+      
+      const response = await api.get(url);
+      console.log(response);
+      const project = response.data;
+      setMyData(project);
+  
+      console.log(`${url}reviews/`);
+      try {
+        const reviewData = await api.get(`${url}reviews/`);
+        console.log(reviewData);
+        console.log(reviewData.data);
+        setReviews(reviewData.data);
+      } catch (err) {
+        // handle errors for this specific await statement
+        console.error("Error fetching reviews:", err);
+        setIsError(err);
+      }
+
     } catch (err) {
+      // handle errors for the entire try block
+      console.error("Error fetching project:", err);
       setIsError(err);
     }
+  }
+
+  const [reviewBody, setReviewBody] = useState("");
+
+  const reviewTest = {
+    "body": "This is the 4th review",
+  }
+
+  const createReview = async () => {
+    console.log(reviewBody);
+    const response = await api.post(`${url}reviews/create/`, { "body": reviewBody });
+    console.log(response);
   }
 
   useEffect(() => {
     fetchProject(); 
   }, [])
 
-  const reviewTest = {
-    "body": "This is my very honest review",
-    "value": null
-  }
-  const createReview = async () => {
-    console.log(`${url}reviews/create`);
-    const response = api.post(`${url}reviews/create`, reviewTest);
-    console.log(response);
-  }
 
   return (
     <>
@@ -86,7 +99,7 @@ export const IndividualProject = (props) => {
                     <p className={styles.feedback}><b>Feedback</b></p>
                     <p className={styles.feedbackInfo}>{props.feedbackCount} Positive Feedback (80 votes)</p>
 
-                    <textarea className={styles.commentSection} id=""></textarea>
+                    <textarea className={styles.commentSection} id="" onChange={(e) => {setReviewBody(e.target.value);}}></textarea>
 
                     <button className={styles.commentBtn} onClick={createReview}>Comment</button><br />
 
