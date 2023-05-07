b0349b83d60ce564dc10ee187a856dcb508bb82a
Aryan-Sawale <aryansawale3@gmail.com>
2023-05-08 00:01:17 +0530

    add and delete projects

--- src/components/ProjectForm/ProjectForm.jsx
+++ src/components/ProjectForm/ProjectForm.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './ProjectForm.module.css'
+import { useAxios } from "../../utils/useAxios"
+import { baseURL } from '../../utils/config'
 
 export const ProjectForm = () => {
+
+  const api = useAxios();
+
+  const dummyData = {
+    title: "Title of my asdasdlife",
+    description: "This is some descriptionnnnnn",
+    demoLink: "https://github.com/atharvabhide/DevFinder/tree/dev/backend",
+    sourceLink: "https://github.com/atharvabhide/DevFinder/tree/dev/backend",
+  }
+
+  const [selectedFile, setSelectedFile] = useState(null); 
+  const [title, settitle] = useState("");
+  const [description, setdescription] = useState("");
+  const [demoLink, setDemoLink] = useState("");
+  const [sourceLink, setSourceLink] = useState("");
+
+  const addProject = async () => {
+    const formData = new FormData();
+
+    formData.append("title", dummyData.title);
+    formData.append("description", dummyData.description);
+    formData.append("demoLink", dummyData.demoLink);
+    formData.append("sourceLink", dummyData.sourceLink);
+    formData.append("featuredImage", selectedFile);
+
+    const response = await api.post(`${baseURL}project-api/projects/create/`, formData);
+    console.log(response);
+  }
+
   return (
     <>
         <div className={styles.wrapper}>
-            <form action="" className={styles.formContainer}>
+            <form action="" className={styles.formContainer} onSubmit={(e) => {e.preventDefault();}}>
                 <input className={styles.inputField} type="text"  placeholder='Title' /><br />
                 {/* <textarea className={styles.bio} name="description" id="" cols="30" rows="10" placeholder='Description'></textarea> */}
                 <input className={styles.inputField} type="text"  placeholder='Username' /><br />
-                <input className={styles.inputField} type="file" placeholder='Hello' multiple /><br />
+                <input className={styles.inputField} type="file" placeholder='Hello' onChange={(e) => {console.log(e.target.files[0]); setSelectedFile(e.target.files[0]);}} /><br />
                 <input className={styles.inputField} type="url" placeholder='Demo Link' /><br />
                 <input className={styles.inputField} type="url"  placeholder='Source Code' />
 
@@ -20,7 +51,7 @@ export const ProjectForm = () => {
                 </div>
 
 
-                <input type="submit" value="Submit"></input>
+                <input type="submit" value="Submit" onClick={addProject}></input>
                 
                 
             </form>
