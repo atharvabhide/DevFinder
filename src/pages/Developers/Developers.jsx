a43ea10067c8b89e7f84c86abd4fb3bbc428a3ec
Aryan Sawale <aryansawale3@gmail.com>
2023-05-09 01:44:08 +0530

    fix conflict

--- src/pages/Developers/Developers.jsx
+++ src/pages/Developers/Developers.jsx
@@ -9,12 +9,17 @@ import { Link } from 'react-router-dom'
 import { SearchBar } from '../../components/SearchBar/SearchBar'
 import axios from 'axios'
 import { baseURL } from '../../utils/config'
+import { Pagination } from '../../components/Pagination/Pagination'
 
 
 export const Developers = () => {
 
   const [users, setUsers] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [data, setData] = useState([]);
+  const itemsPerPage =6;
+  const totalPages = Math.ceil(users.length / itemsPerPage);
 
   const fetchDevelopers = async () => {
     const response = await axios.get(`${baseURL}user-api/profiles/`);
@@ -27,68 +32,58 @@ export const Developers = () => {
     fetchDevelopers();
   }, [])
 
-  const filteredUsers = users.filter((user) =>
-    user.username.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const renderData = () => {
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  return (
+    users.slice(startIndex, endIndex).map((item, index) => {
+    if (!item) {
+      return null;
+    }
+    return (
+      // render each item
+      <>
+      <Link to="/developers/developer" key={item.url} state={{url: item.url}}>
+            <DevCard
+              
+              name={item.username}
+              position={item.shortIntro}
+              image={item.profileImage}
+              bio={item.bio}
+ 
+            />
+            </Link>
+         
+      
+
+      </>
+    );
+  
+  }));
+};
+
 
   return (
     <>
       <div className={styles.wrapper}>
         <div className={styles.header}>
             Search For Developers
-            <SearchBar setSearchQuery={setSearchQuery}/>
+            <SearchBar />
         </div>  
 
         <div className={styles.developers}>
 
-          {filteredUsers.map((items) => (
-            <Link to="/developers/developer" key={items.url} state={{url: items.url}}>
-            <DevCard
-              
-              name={items.username}
-              position={items.shortIntro}
-              image={items.profileImage}
-              bio={items.bio}
-
-              // position="ML/Backend Developer"
-              // bio="The air was crisp and cool, with a hint of the upcoming autumn season. In the distance, a small stream meandered its way through the fields, glistening like a ribbon of silver in the morning light. It was a peaceful and idyllic scene, one that seemed to invite contemplation and reflection."
-              // skill1="Python" 
-              // skill2="MongoDB" 
-              // skill3="Tensorflow" 
-              // skill4="React" 
-              // skill5="Data Science" 
-            />
-            </Link>
-          ))}
-
-          
-          
-          {/* <Link to="/developers/developer">
-          <DevCard
-          image={Ak}
-            name="Aakash Kasabekar"
-            position="Frontend Developer"
-            bio="The air was crisp and cool, with a hint of the upcoming autumn season. In the distance, a small stream meandered its way through the fields, glistening like a ribbon of silver in the morning light. It was a peaceful and idyllic scene, one that seemed to invite contemplation and reflection." 
-          />
-          </Link>
-          <Link to="/developers/developer">
-          <DevCard
-          image={Dhanya}
-            name="Dhananjay Deshmukh"
-            position="ML/Backend Developer"
-            bio="The air was crisp and cool, with a hint of the upcoming autumn season. In the distance, a small stream meandered its way through the fields, glistening like a ribbon of silver in the morning light. It was a peaceful and idyllic scene, one that seemed to invi
-            te contemplation and reflection." 
-          />
-          </Link>
-          <Link to="/developers/developer">
-          <DevCard
-          
-            name="Atharva Bhide"
-            position="ML/Backend Developer"
-            bio="The air was crisp and cool, with a hint of the upcoming autumn season. In the distance, a small stream meandered its way through the fields, glistening like a ribbon of silver in the morning light. It was a peaceful and idyllic scene, one that seemed to invite contemplation and reflection." 
-          />
-          </Link> */}
+        {renderData()}
         </div>
+        <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+        />
     </div>
     
     <Footer />
