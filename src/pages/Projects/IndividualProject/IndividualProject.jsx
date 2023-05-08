ceff10a965c95857a69c8bb29df2f31ddc2c8a1b
Aryan Sawale <aryansawale3@gmail.com>
2023-05-08 23:17:00 +0530

    reviews with profile image in individual projects

--- src/pages/Projects/IndividualProject/IndividualProject.jsx
+++ src/pages/Projects/IndividualProject/IndividualProject.jsx
@@ -10,6 +10,7 @@ import { Modal } from '../../../components/Modal/Modal'
 import { useAxios } from '../../../utils/useAxios'
 
 import { ProjectReview } from '../../../components/ProjectReview/ProjectReview'
+import { baseURL } from '../../../utils/config';
 
 export const IndividualProject = (props) => {
 
@@ -18,6 +19,7 @@ export const IndividualProject = (props) => {
     const location = useLocation();
     const [show, setShow] = useState(false);
     const [reviews, setReviews] = useState([]);
+    const [reviewProfiles, setReviewProfiles] = useState([]);
 
     // console.log(location);
     const url = location.state?.url;
@@ -43,7 +45,7 @@ export const IndividualProject = (props) => {
         // const reversedReviews = reviewData.data.results.reverse();
         // setReviews(reversedReviews);
 
-        setReviews(reviewData.data.results)
+        setReviews(reviewData.data)
       } catch (err) {
         // handle errors for this specific await statement
         console.error("Error fetching reviews:", err);
@@ -115,14 +117,16 @@ export const IndividualProject = (props) => {
 
                     <p className={styles.feedback}><b>Reviews</b></p>
 
-                    {reviews.map((items) => (
-            
+                    {reviews?.map((items) => (
+                      
+                      <>
                       <ProjectReview
 
-                        image={Profile}
-                        username={items.owner}
+                        image={`${baseURL}${items.owner?.profileImage}`}
+                        username={items.owner?.username}
                         comment={items.body}
                       />
+                      </>
             
                     ))}
 
