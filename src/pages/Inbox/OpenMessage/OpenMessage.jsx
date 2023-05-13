410e6ea251fbac24c02971af7c67e88c1f9863cd
Aryan Sawale <aryansawale3@gmail.com>
2023-05-14 02:45:48 +0530

    fix Open Message

--- src/pages/Inbox/OpenMessage/OpenMessage.jsx
+++ src/pages/Inbox/OpenMessage/OpenMessage.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
-import styles from './OpenMessage.module.css'
-import { useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { useAxios } from '../../../utils/useAxios';
-import {RxCross2} from 'react-icons/rx'
-import { useContext } from 'react';
-import { AuthContext } from '../../../context/AuthContext';
-
+import React from "react";
+import styles from "./OpenMessage.module.css";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useAxios } from "../../../utils/useAxios";
+import { RxCross2 } from "react-icons/rx";
+import { useContext } from "react";
+import { AuthContext } from "../../../context/AuthContext";
 
 export const OpenMessage = (props) => {
-    if (!props.show){
-        return null
-    }
+  if (!props.show) {
+    return null;
+  }
 
-    const { currentUUID } = useContext(AuthContext);
+  const { currentUUID } = useContext(AuthContext);
   console.log(currentUUID);
-  
+
   // const [message, setMessage] = useState();
-  
-  
+
   // const api = useAxios();
-  
+
   // const fetchMessages = async () => {
   //   const response = await api.get(`user-api/profiles/${currentUUID}/messages/`);
   //   console.log(response);
@@ -33,24 +31,29 @@ export const OpenMessage = (props) => {
   //   fetchMessages();
   // }, [])
 
-    
-
   return (
     <div className={styles.wrapper} onClick={props.onClose}>
-        <div className={styles.msgContainer} onClick={e => e.stopPropagation()}>
-            <div className={styles.header}>
-              <h1>From <i>{props.name}</i>,</h1>
-              <h3 htmlFor="" style={{marginTop: '1em'}}>Subject</h3>
-              <p>{props.subject}</p>
-              <h3 htmlFor="" style={{marginTop: '1em'}}>Message</h3>
-              <p>{props.message}</p>
+      <div className={styles.msgContainer} onClick={(e) => e.stopPropagation()}>
+        <div className={styles.header}>
+          <h1>
+            From <i>{props.name}</i>,
+          </h1>
+          <h3 htmlFor="" style={{ marginTop: "1em" }}>
+            Subject
+          </h3>
+          <p>{props.subject}</p>
+          <h3 htmlFor="" style={{ marginTop: "1em" }}>
+            Message
+          </h3>
+          <div className={styles.messageBody}>{props.message}</div>
 
-             
-              <RxCross2 onClick={props.onClose} size={22} className={styles.closeButton} />
-
-            </div>
-
+          <RxCross2
+            onClick={props.onClose}
+            size={22}
+            className={styles.closeButton}
+          />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
