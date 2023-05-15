7be1b7c75e674e89ce972e659b4d0b9e61804538
Aryan Sawale <aryansawale3@gmail.com>
2023-05-15 23:53:19 +0530

    prettier formatting

--- src/pages/Projects/IndividualProject/IndividualProject.jsx
+++ src/pages/Projects/IndividualProject/IndividualProject.jsx
@@ -1,43 +1,40 @@
-import React from 'react'
-import styles from './IndividualProject.module.css'
-import { Link, useLocation } from 'react-router-dom'
-import ProjectImage from '../../../assets/banner5.jpg'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import Profile from '../../../assets/dhanya2.jpg'
+import React from "react";
+import styles from "./IndividualProject.module.css";
+import { Link, useLocation } from "react-router-dom";
+import ProjectImage from "../../../assets/banner5.jpg";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Profile from "../../../assets/dhanya2.jpg";
 
-import { Modal } from '../../../components/Modal/Modal'
-import { useAxios } from '../../../utils/useAxios'
+import { Modal } from "../../../components/Modal/Modal";
+import { useAxios } from "../../../utils/useAxios";
 
-import { ProjectReview } from '../../../components/ProjectReview/ProjectReview'
-import { baseURL } from '../../../utils/config';
+import { ProjectReview } from "../../../components/ProjectReview/ProjectReview";
+import { baseURL } from "../../../utils/config";
 
 export const IndividualProject = (props) => {
+  const [myData, setMyData] = useState([]);
+  const [isError, setIsError] = useState("");
+  const location = useLocation();
+  const [show, setShow] = useState(false);
+  const [reviews, setReviews] = useState([]);
+  const [reviewProfiles, setReviewProfiles] = useState([]);
+  const [tags, setTags] = useState([]);
 
-    const [myData, setMyData] = useState([]);
-    const [isError, setIsError] = useState("");
-    const location = useLocation();
-    const [show, setShow] = useState(false);
-    const [reviews, setReviews] = useState([]);
-    const [reviewProfiles, setReviewProfiles] = useState([]);
-    const [tags, setTags] = useState([]);
+  // console.log(location);
+  const url = location.state?.url;
+  // console.log(url);
 
-    // console.log(location);
-    const url = location.state?.url;
-    // console.log(url);
+  const api = useAxios();
 
-    const api = useAxios();
-    
-  
   const fetchProject = async () => {
     try {
-      
       const response = await api.get(url);
       console.log(response);
       const project = response.data;
       setMyData(project);
-      setTags(project.tags.map((tag) => (tag.name)));
-  
+      setTags(project.tags.map((tag) => tag.name));
+
       console.log(`${url}reviews/`);
       try {
         const reviewData = await api.get(`${url}reviews/`);
@@ -47,123 +44,140 @@ export const IndividualProject = (props) => {
         // const reversedReviews = reviewData.data.results.reverse();
         // setReviews(reversedReviews);
 
-        setReviews(reviewData.data)
+        setReviews(reviewData.data);
       } catch (err) {
         // handle errors for this specific await statement
         console.error("Error fetching reviews:", err);
         setIsError(err);
       }
-
     } catch (err) {
       // handle errors for the entire try block
       console.error("Error fetching project:", err);
       setIsError(err);
     }
-  }
+  };
 
   const [reviewBody, setReviewBody] = useState("");
 
   const reviewTest = {
-    "body": reviewBody,
-  }
+    body: reviewBody,
+  };
 
   const [shouldFetchProject, setShouldFetchProject] = useState(false);
 
   const createReview = async () => {
     console.log(reviewBody);
     const reviewTest = {
-      "comment": reviewBody,
-    }
+      comment: reviewBody,
+    };
     const responseNSFW = await api.post(`project-api/review/mod/`, reviewTest);
     console.log("nsfw", responseNSFW);
-    if (responseNSFW.data.prediction === "Review is clean")
-    {
-      const response = await api.post(`${url}reviews/create/`, { "body": reviewBody });
+    if (responseNSFW.data.prediction === "Review is clean") {
+      const response = await api.post(`${url}reviews/create/`, {
+        body: reviewBody,
+      });
       console.log(response);
       setShouldFetchProject(true);
-
+    } else {
+      alert("NSFW comments not allowed");
     }
-    else
-    {
-      alert("NSFW comments not allowed")
-    }
-
-  }
+  };
 
   useEffect(() => {
-    fetchProject(); 
+    fetchProject();
 
     setShouldFetchProject(false);
-  }, [shouldFetchProject])
-
+  }, [shouldFetchProject]);
 
   return (
     <>
-        <div className={styles.wrapper}>
-            <div className={styles.project}>
-                <div className={styles.projectTools}>
-                    <h1>Tools And Stacks</h1><br />
-                    <div className={styles.toolSection}>
-                        {tags.map((tag) => (
-                          <>
-                            <button className={styles.toolBtn}>{tag}</button>
-                          </>
-                        ))}
-                        {/* <button className={styles.toolBtn}>React</button>
+      <div className={styles.wrapper}>
+        <div className={styles.project}>
+          <div className={styles.projectTools}>
+            <h1>Tools And Stacks</h1>
+            <br />
+            <div className={styles.toolSection}>
+              {tags.map((tag) => (
+                <>
+                  <button className={styles.toolBtn}>{tag}</button>
+                </>
+              ))}
+              {/* <button className={styles.toolBtn}>React</button>
                         <button className={styles.toolBtn}>React</button>
                         <button className={styles.toolBtn}>React</button>
                         <button className={styles.toolBtn}>React</button> */}
-                    </div>
-                    <div className={styles.projectLinks}>
-                        <Link to="#" style={{color: '#eb7724'}}>Demo Link</Link><br /><br />
-                        <Link to="#" style={{color: '#eb7724'}}>Source Code</Link>
-                    </div>
-                </div>
-                <div className={styles.projectInfo}>
-                    <img src={myData.featuredImage} className={styles.projectImage} alt="" />
-                    <hr />
-                    <p className={styles.projectTitle}><b>{myData.title}</b></p>
-                    <p className={styles.projectDeveloper}><i>By {myData.owner}</i></p><br />
-                    <p className={styles.aboutProject}><b>ABOUT THE PROJECT</b></p>
-                    <p className={styles.aboutInfo}>{myData.description}</p>
-                    
-                    <hr />
-                    <p className={styles.feedback}><b>Feedback</b></p>
-                    <p className={styles.feedbackInfo}>{myData.voteRatio}% Positive Feedback ({myData.voteTotal} votes)</p>
+            </div>
+            <div className={styles.projectLinks}>
+              <Link to="#" style={{ color: "#eb7724" }}>
+                Demo Link
+              </Link>
+              <br />
+              <br />
+              <Link to="#" style={{ color: "#eb7724" }}>
+                Source Code
+              </Link>
+            </div>
+          </div>
+          <div className={styles.projectInfo}>
+            <img
+              src={myData.featuredImage}
+              className={styles.projectImage}
+              alt=""
+            />
+            <hr />
+            <p className={styles.projectTitle}>
+              <b>{myData.title}</b>
+            </p>
+            <p className={styles.projectDeveloper}>
+              <i>By {myData.owner}</i>
+            </p>
+            <br />
+            <p className={styles.aboutProject}>
+              <b>ABOUT THE PROJECT</b>
+            </p>
+            <p className={styles.aboutInfo}>{myData.description}</p>
 
-                    <textarea className={styles.commentSection} id="" onChange={(e) => {setReviewBody(e.target.value);}}></textarea>
+            <hr />
+            <p className={styles.feedback}>
+              <b>Feedback</b>
+            </p>
+            <p className={styles.feedbackInfo}>
+              {myData.voteRatio}% Positive Feedback ({myData.voteTotal} votes)
+            </p>
 
-                    <button className={styles.commentBtn} onClick={createReview}>Comment</button><br />
+            <textarea
+              className={styles.commentSection}
+              id=""
+              onChange={(e) => {
+                setReviewBody(e.target.value);
+              }}
+            ></textarea>
 
-                    <p className={styles.feedback}><b>Reviews</b></p>
+            <button className={styles.commentBtn} onClick={createReview}>
+              Comment
+            </button>
+            <br />
 
-                    {reviews?.map((items) => (
-                      
-                      <>
-                      <ProjectReview
+            <p className={styles.feedback}>
+              <b>Reviews</b>
+            </p>
 
-                        image={`${baseURL}${items.owner?.profileImage}`}
-                        username={items.owner?.username}
-                        comment={items.body}
-                      />
-                      </>
-            
-                    ))}
+            {reviews?.map((items) => (
+              <>
+                <ProjectReview
+                  image={`${baseURL}${items.owner?.profileImage}`}
+                  username={items.owner?.username}
+                  comment={items.body}
+                />
+              </>
+            ))}
 
-                    
-                    
-
-
-
-                    {/* <button className={styles.commentBtn} onClick={() => setShow(true)}>Delete</button>
+            {/* <button className={styles.commentBtn} onClick={() => setShow(true)}>Delete</button>
 
                     <Modal onClose={() => setShow(false)} show={show} /> */}
-
-                </div>
-
-            </div>
-
+          </div>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
--- src/pages/Recommendations/Recommendations.jsx
+++ src/pages/Recommendations/Recommendations.jsx
@@ -1,54 +1,51 @@
-import React, { useEffect } from 'react'
-import styles from './Recommendations.module.css'
-import { DevCard } from '../../components/DevCard/DevCard'
-import { useAxios } from '../../utils/useAxios';
-import { AuthContext } from '../../context/AuthContext';
-import { useContext, useState } from 'react';
-import { baseURL } from '../../utils/config';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import styles from "./Recommendations.module.css";
+import { DevCard } from "../../components/DevCard/DevCard";
+import { useAxios } from "../../utils/useAxios";
+import { AuthContext } from "../../context/AuthContext";
+import { useContext, useState } from "react";
+import { baseURL } from "../../utils/config";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export const Recommendations = () => {
-    const {currentUUID} = useContext(AuthContext);
+  const { currentUUID } = useContext(AuthContext);
 
-    const api = useAxios();
+  const api = useAxios();
 
-    const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
-    const fetchReccommended = async () => {
-        const response = await api.get(`user-api/similar/`);
-        console.log(response);
-        setUsers(response.data);
-    }
+  const fetchReccommended = async () => {
+    const response = await api.get(`user-api/similar/`);
+    console.log(response);
+    setUsers(response.data);
+  };
 
-    useEffect(() => {
-        fetchReccommended();
-    }, [])
+  useEffect(() => {
+    fetchReccommended();
+  }, []);
 
   return (
     <>
-        <div className={styles.header}>
-            Recommended Developers
-            
-        </div>  
-    <div className={styles.developers}>
+      <div className={styles.header}>Recommended Developers</div>
+      <div className={styles.developers}>
         {users?.map((user) => (
-            <>
-            <Link to="/developers/developer" key={user.url} state={{url: user.url}}>
-                <DevCard 
-                    name={user.username}
-                    position={user.shortIntro}
-                    bio={user.bio}
-                    image={user.profileImage}
-                />
+          <>
+            <Link
+              to="/developers/developer"
+              key={user.url}
+              state={{ url: user.url }}
+            >
+              <DevCard
+                name={user.username}
+                position={user.shortIntro}
+                bio={user.bio}
+                image={user.profileImage}
+              />
             </Link>
-            </>
+          </>
         ))}
-        
-        
-        
-    </div>
+      </div>
     </>
-
-  )
-}
+  );
+};
