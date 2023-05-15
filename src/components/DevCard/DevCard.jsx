947aad9f7a8e4fd39574c91f654b5cebcedf04eb
Aakash <aakashkasabekar@gmail.com>
2023-05-16 02:07:34 +0530

    minor changes

--- src/components/DevCard/DevCard.jsx
+++ src/components/DevCard/DevCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {useContext, useState } from 'react'
 import styles from './DevCard.module.css';
 import { Link } from 'react-router-dom';
 import {FaLinkedinIn} from 'react-icons/fa'
@@ -8,6 +8,9 @@ import {FiUser} from 'react-icons/fi'
 import Banner from '../../assets/banner4.jpg'
 import Dp from '../../assets/nalla.jpg'
 import defaultImage from '../../assets/default-image.svg'
+import {SiHashnode} from 'react-icons/si'
+import {BsTwitter} from 'react-icons/bs'
+
 
 export const DevCard = (props) => {
     
@@ -40,9 +43,9 @@ export const DevCard = (props) => {
                 <button className={styles.skillBtn} style={props.skill5 ? {backgroundColor: '#5670d7'} : {display: 'none'}}>{props.skill5}</button>
             </div>
             <div className={styles.socialLinks}>
-                <Link to="#" className={styles.link}><FaLinkedinIn size={23} /></Link>
-                <Link to="#" className={styles.link}><SiGmail size={23} /></Link>
-                <Link to="#" className={styles.link}><GoMarkGithub size={23} /></Link>
+                <Link to={props.hashnodeLink} className={styles.link}><SiHashnode size={23} /></Link>
+                <Link to={props.twitterLink} className={styles.link}><BsTwitter size={23} /></Link>
+                <a href={props.githubLink} target='_blank' className={styles.link}><GoMarkGithub size={23} /></a>
             </div>
             </div>
 
